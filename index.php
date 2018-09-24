1088123bd5837e22e275c996412decf6ac31ed33
barbosafernandes <warlley_1996@hotmail.com>
2018-09-24 20:16:57 -0300

    primeiro commit QR cod

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="qr.php" method="get"> 
+	<input type="text" name="nome">
+	<input type="submit" name="">
+</form>
+</body>
+</html>
